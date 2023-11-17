cc418e21ed247a0c9c10818b970eedeb1d8aebaa
Andrei Aiurov <aiuroand@fit.cvut.cz>
2023-11-17 19:23:56 +0100

    1st try

--- homework02/filtering/filtering.py
+++ homework02/filtering/filtering.py
@@ -1,5 +1,5 @@
+""" Filtering function """
 import numpy as np
-
 
 def apply_filter(image: np.array, kernel: np.array) -> np.array:
     """ Apply given filter on image """
@@ -8,5 +8,42 @@ def apply_filter(image: np.array, kernel: np.array) -> np.array:
     # A given filter has to be 2 dimensional and square
     assert kernel.ndim == 2
     assert kernel.shape[0] == kernel.shape[1]
-
-    # TODO
+    newimage = image.copy()
+    if image.ndim == 3:
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                for c in range(image.ndim):
+                    s = 0
+                    for k1 in range(-(kernel.shape[0]//2), kernel.shape[1]//2 + 1):
+                        for k2 in range(-(kernel.shape[0]//2), kernel.shape[1]//2 + 1):
+                            # print(k1, k2)
+                            if ((i+k1 < 0) or (j+k2 < 0) or (i+k1 >= image.shape[0]) or (j+k2 >= image.shape[1])):
+                                s = s + 0
+                            else:
+                                s = s + (kernel[kernel.shape[0]//2+k1][kernel.shape[0]//2+k2] * image[i+k1][j+k2][c])
+                    # print(s)
+                    if s > 255:
+                        newimage[i][j][c] = 255
+                    elif s < 0:
+                        newimage[i][j][c] = 0
+                    else:
+                        newimage[i][j][c] = s
+    else:
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                s = 0
+                for k1 in range(-(kernel.shape[0]//2), kernel.shape[1]//2 + 1):
+                    for k2 in range(-(kernel.shape[0]//2), kernel.shape[1]//2 + 1):
+                        # print(k1, k2)
+                        if ((i+k1 < 0) or (j+k2 < 0) or (i+k1 >= image.shape[0]) or (j+k2 >= image.shape[1])):
+                            s = s + 0
+                        else:
+                            s = s + (kernel[kernel.shape[0]//2+k1][kernel.shape[0]//2+k2] * image[i+k1][j+k2])
+                # print(s)
+                if s > 255:
+                    newimage[i][j] = 255
+                elif s < 0:
+                    newimage[i][j] = 0
+                else:
+                    newimage[i][j] = s
+    return newimage
